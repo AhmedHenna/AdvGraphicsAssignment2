6b6097c8fa574bb659201996de704df7b0933abc
AhmedHenna <71960514+AhmedHenna@users.noreply.github.com>
2022-04-21 16:24:03 +0200

    Finished Zoom Camera

---
+++ Assignement 2/Assets/CameraZoom.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    int zoom = 60; 
+    int normal = 60; 
+    float smooth = 5;
+
+    private bool isZoomed = false;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1) && zoom > 15)
+        {   
+            Debug.Log("Before" + zoom);
+            isZoomed = true;
+            zoom -= 15;
+            Debug.Log(zoom);
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, zoom, Time.deltaTime * smooth);
+
+        }
+        if(Input.GetMouseButtonDown(0) && zoom !=60){
+            isZoomed = true;
+            zoom += 15;
+            Debug.Log(zoom);
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, zoom, Time.deltaTime * smooth);
+        }
+        if(Input.GetMouseButtonDown(2)){
+            isZoomed = false;
+        }
+
+
+        if(isZoomed)
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, zoom, Time.deltaTime * smooth);
+        }
+
+        if(!isZoomed)
+        {
+             GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, normal, Time.deltaTime * smooth);
+            zoom = 60;
+
+        }
+    }
+
+
+
+
+
+}
